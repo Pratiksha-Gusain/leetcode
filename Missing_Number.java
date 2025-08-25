34ee2e69d82b5995314de4a1c5a0c49c05b5e14d
Pratiksha Gusain <pratikshagusain07@gmail.com>
2025-08-25 21:39:21 +0530

    add

---
+++ Missing_Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum= sum+nums[i];
+        }     
+        int total =(nums.length*(nums.length+1))/2;
+        int num= total-sum;
+        return num;
+    }
+}
