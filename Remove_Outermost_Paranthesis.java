bda9a070b0f0e4afb5e8ee54d532a696ed46fa07
Pratiksha Gusain <pratikshagusain07@gmail.com>
2025-08-29 08:12:36 +0530

    added

---
+++ Remove_Outermost_Paranthesis.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count=0;
+        StringBuilder str = new StringBuilder();
+        for(int i=0; i<s.length();i++){
+            if(s.charAt(i)==')')
+                count--;
+            if(count!=0)
+                str.append(s.charAt(i));
+            if(s.charAt(i)=='(')
+                count++;
+        }
+        return str.toString();                
+        
+    }
+}
