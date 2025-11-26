71fb77e34e4588c29ef22f968f9cdc5c1b2552a1
Pratiksha-Gusain <144364758+Pratiksha-Gusain@users.noreply.github.com>
2025-11-26 22:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (11.80%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,29 +1,15 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rowidx = potentialRow(matrix,target);
-        if(rowidx!=-1) return BinarySearch(rowidx, matrix, target);
-        else return false;
-    }
-    public int potentialRow(int[][] matrix, int target){
-        int low =0;
-        int high = matrix.length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(target>=matrix[mid][0] && target<=matrix[mid][matrix[0].length-1]) return mid;
-            else if (target>matrix[mid][0]) low=mid+1;
-            else if (target<matrix[mid][0]) high=mid-1;
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int row=0;
+        int col=m-1;
+        while(row<n && col>=0){
+            if(matrix[row][col]==target) return true;
+            else if(target<matrix[row][col]) col--;
+            else row++;
         }
-        return -1;
-    }
-    public boolean BinarySearch(int rowidx, int[][]matrix, int target){
-            int low=0;
-            int high=matrix[rowidx].length-1;
-            while(low<=high){
-                int mid = low+(high-low)/2;
-                if(matrix[rowidx][mid]==target) return true;
-                else if(matrix[rowidx][mid]<target) low=mid+1;
-                else high=mid-1;
-            }
-            return false;
+        return false;
+        
     }
 }
