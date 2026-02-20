8391cb044ae42b99ccd05ed8b24784dbdd64284e
Pratiksha-Gusain <144364758+Pratiksha-Gusain@users.noreply.github.com>
2026-02-20 20:59:36 +0530

    Time: 5 ms (97.05%), Space: 44 MB (77.11%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] freq1 = new int[26];
+        int[] freq2= new int[26];
+        int n = s1.length();
+        int m = s2.length();
+        if(n>m) return false;
+        for(int i=0;i<n;i++){
+            freq1[s1.charAt(i)-'a']++;
+        }
+        int left =0;
+        for(int right = 0; right < m; right++){
+            freq2[s2.charAt(right)- 'a']++;
+            if( right - left + 1 > n){
+                freq2[s2.charAt(left)- 'a']--;
+                left++;
+            }
+            if(right - left + 1 == n && Arrays.equals(freq1,freq2))
+                return true;
+        }
+        return false;
+    }
+}
