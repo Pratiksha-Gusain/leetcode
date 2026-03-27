39e36ee0634be9d869e15947be984a85e2496c31
Pratiksha-Gusain <144364758+Pratiksha-Gusain@users.noreply.github.com>
2026-03-27 23:44:58 +0530

    Time: 0 ms (100%), Space: 43.5 MB (57.26%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,22 +1,24 @@
 class Solution {
-    static int MOD=1000000007;
-     public static int solve(int i,int j,int [][]arr,int [][]dp){
-        if(i < 0 || j < 0) return 0;
-        if(arr[i][j] == 1) return 0;
-        if(i == 0 && j == 0) return 1;
-        if(dp[i][j]!= -1) return dp[i][j];
-        int up = solve(i-1,j,arr,dp);
-        int left = solve(i,j-1,arr,dp);
-        dp[i][j] = (up+left);
-        return dp[i][j];
-    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int n=obstacleGrid.length;
         int m=obstacleGrid[0].length;
-        int dp[][]=new int[n][m];
+        int[] prev= new int[m];
         for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+            int[] curr= new int[m];
+            for(int j=0;j<m;j++){
+                if(obstacleGrid[i][j]==1)curr[j]=0;
+                else if(i==0 && j==0)curr[j]=1;
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=prev[j];
+                    if(j>0) left= curr[j-1];
+                    curr[j]=up+left;
+                }
+            }
+            prev=curr;
         }
-        return solve(n-1,m-1,obstacleGrid,dp);
+        return prev[m-1];
+        
     }
 }
