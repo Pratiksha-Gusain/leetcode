a1d6af5c2c737433fa7ab7e2c17b2f07a851b3a7
Pratiksha-Gusain <144364758+Pratiksha-Gusain@users.noreply.github.com>
2026-05-11 15:45:08 +0530

    Time: 4 ms (99.42%), Space: 47.8 MB (36.77%) - LeetHub

--- 3922-minimum-flips-to-make-binary-string-coherent/3922-minimum-flips-to-make-binary-string-coherent.java
+++ 3922-minimum-flips-to-make-binary-string-coherent/3922-minimum-flips-to-make-binary-string-coherent.java
@@ -1,26 +1,14 @@
 class Solution {
     public int minFlips(String s) {
+        int[] f = {0, -1};
         int n = s.length();
-        int ans = n;
-        for(int i = 0; i <=n; i++){
-            ans = Math.min(ans, changes(s,i,'0','1'));
-            ans = Math.min(ans, changes(s,i,'1','0'));
-        }
-        return ans;
+
+        for (int i = 0; i < n; i++)
+            f[s.charAt(i) & 1]++;
+
+        if (f[0] == 0 || f[1] == -1) return 0;
+
+        return Math.min(f[0], f[1] - (s.charAt(0) & s.charAt(n - 1) & 1)
+        );
     }
-        
-        private int changes(String s, int split, char left, char right) {
-        int flips = 0;
-        for (int i = 0; i < split; i++) {
-            if (s.charAt(i) != left) {
-                flips++;
-            }
-        }
-        for (int i = split; i < s.length(); i++) {
-            if (s.charAt(i) != right) {
-                flips++;
-            }
-        }
-        return flips;
-        }
 }
