c40203dc363343d5cad7fa4bf18aef18b901b2c9
Pratiksha-Gusain <144364758+Pratiksha-Gusain@users.noreply.github.com>
2026-08-20 22:39:22 +0530

    Time: 19 ms (39.47%), Space: 47.1 MB (21.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,16 +1,30 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount<1) return 0;
-        int[] minCoins= new int[amount+1];
-        for(int i=1;i<=amount;i++){
-            minCoins[i]= Integer.MAX_VALUE;
-            for(int coin:coins){
-                if(coin<=i && minCoins[i-coin]!=Integer.MAX_VALUE){
-                    minCoins[i]=Math.min(minCoins[i],1+minCoins[i-coin]);
-                }
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+
+        for(int tar = 0; tar <= amount; tar++){
+            if(tar%coins[0] == 0){
+                dp[0][tar] = tar/coins[0];
+            }
+            else{
+                dp[0][tar] = Integer.MAX_VALUE;
             }
         }
-        if(minCoins[amount]==Integer.MAX_VALUE) return -1;
-            return minCoins[amount];
+
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= amount; j++){
+                int notTake = dp[i-1][j];
+                int take = Integer.MAX_VALUE;
+                if(coins[i] <= j && dp[i][j-coins[i]] != Integer.MAX_VALUE){
+                    take = 1 +  dp[i][j-coins[i]];
+                }
+                dp[i][j] = Math.min(take, notTake);
+            }
+        }
+        int ans = dp[n-1][amount];
+        if( ans != Integer.MAX_VALUE) return ans;
+        return -1;
+
     }
 }
