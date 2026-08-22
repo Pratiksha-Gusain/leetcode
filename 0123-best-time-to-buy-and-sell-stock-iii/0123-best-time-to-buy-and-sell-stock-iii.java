1175ea0a4821637f6ef8663e3e849b4fbfdf222a
Pratiksha-Gusain <144364758+Pratiksha-Gusain@users.noreply.github.com>
2026-08-22 17:26:18 +0530

    Time: 16 ms (49.52%), Space: 75.7 MB (88.81%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -7,12 +7,12 @@ class Solution {
         for (int ind = n - 1; ind >= 0; ind--) {
             for (int buy = 0; buy <= 1; buy++) { 
                 for (int cap = 1; cap <= 2; cap++) {
-                    if (buy == 0) {
-                        cur[buy][cap] = Math.max(ahead[0][cap],
-                                -prices[ind] + ahead[1][cap]);
-                    } else {
+                    if (buy == 1) {
                         cur[buy][cap] = Math.max(ahead[1][cap],
-                                prices[ind] + ahead[0][cap - 1]);
+                                -prices[ind] + ahead[0][cap]);
+                    } else {
+                        cur[buy][cap] = Math.max(ahead[0][cap],
+                                prices[ind] + ahead[1][cap - 1]);
                     }
                 }
             }
@@ -21,6 +21,6 @@ class Solution {
             }
         }
 
-        return ahead[0][2];
+        return ahead[1][2];
     }
 }
