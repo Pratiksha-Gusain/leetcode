3562d4758add840f6c34b83e0fc9f3825403a7ce
Pratiksha-Gusain <144364758+Pratiksha-Gusain@users.noreply.github.com>
2026-08-22 17:27:39 +0530

    Time: 2 ms (71.67%), Space: 45.6 MB (18.14%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -5,17 +5,17 @@ class Solution {
         for (int ind = n - 1; ind >= 0; ind--) {
             for (int buy = 0; buy <= 1; buy++) {
                 for (int cap = 1; cap <= k; cap++) {
-                    if (buy == 0) {
-                        dp[ind][buy][cap] = Math.max(dp[ind + 1][0][cap],
-                                -prices[ind] + dp[ind + 1][1][cap]);
-                    } else {
+                    if (buy == 1) {
                         dp[ind][buy][cap] = Math.max(dp[ind + 1][1][cap],
-                                prices[ind] + dp[ind + 1][0][cap - 1]);
+                                -prices[ind] + dp[ind + 1][0][cap]);
+                    } else {
+                        dp[ind][buy][cap] = Math.max(dp[ind + 1][0][cap],
+                                prices[ind] + dp[ind + 1][1][cap - 1]);
                     }
                 }
             }
         }
-        return dp[0][0][k];
+        return dp[0][1][k];
     }
         
 }
