869b0547ca53f4e02d3093a2ae471a1f4635cc86
Pratiksha-Gusain <144364758+Pratiksha-Gusain@users.noreply.github.com>
2026-09-07 03:01:35 +0530

    Time: 13 ms (91.02%), Space: 49.2 MB (51.93%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -4,25 +4,34 @@ class Solution {
         int n=mat.length;
         int m= mat[0].length;
         Queue<int[]> q = new LinkedList<>();
-        int max = m*n;
+        int[][] vis = new int [n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]==0) q.offer(new int[]{i,j});
-                else mat[i][j]=max;
+                if(mat[i][j]==0){
+                    q.offer(new int[]{i,j,0});
+                    vis[i][j] = 1;
+                } 
+                
             }
         }
+        int[][] dist = new int[n][m];
         int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
         while(!q.isEmpty()){
             int[] curr = q.poll();
+            int row = curr[0];
+            int col = curr[1];
+            int steps = curr[2];
+            dist[row][col] = steps;
             for(int[] i: dir){
-                int r=curr[0]+i[0];
-                int c=curr[1]+i[1];
-                if(r<0||c<0||r>=n||c>=m||mat[r][c]<mat[curr[0]][curr[1]]+1) continue;
-                q.offer(new int[]{r,c});
-                mat[r][c]=mat[curr[0]][curr[1]]+1;
-
+                int r=row+i[0];
+                int c=col+i[1];
+                if(r >=0 && r < n && c >=0 && c < m && vis[r][c]==0){
+                    vis[r][c] = 1;
+                    q.offer(new int[]{r,c, steps+1});
+                    
+                }
             }
         }
-        return mat;
+        return dist;
     }
 }
