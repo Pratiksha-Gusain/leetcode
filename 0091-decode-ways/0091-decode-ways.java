035dbd69eba30539ee0d6c118c7fb52bfe7da80c
Pratiksha-Gusain <144364758+Pratiksha-Gusain@users.noreply.github.com>
2026-09-16 17:40:30 +0530

    Time: 0 ms (100%), Space: 42.9 MB (87.04%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -4,14 +4,13 @@ class Solution {
         int[]dp = new int[n+1];
         dp[0]=1;
         dp[1]=s.charAt(0)=='0'?0:1;
-        for(int i=2;i<=n;i++){
-            int oneDigit = Integer.valueOf(s.substring(i-1,i));
-            int twoDigit = Integer.valueOf(s.substring(i-2,i));
-            if(oneDigit>=1){
-                dp[i]+=dp[i-1];
-            } 
-            if(twoDigit>=10 && twoDigit<=26){
-                dp[i]+=dp[i-2];
+        for (int i = 2; i <= n; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            if (s.charAt(i - 2) == '1' || (s.charAt(i - 2) == '2' && s.charAt(i - 1) < '7')) {
+                dp[i] += dp[i - 2];
             }
         }
         return dp[n];
