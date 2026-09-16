d539c79b856b298f6a722f6fa7e3dd672c53eff1
Pratiksha-Gusain <144364758+Pratiksha-Gusain@users.noreply.github.com>
2026-09-16 14:32:18 +0530

    Time: 0 ms (100%), Space: 43.5 MB (52.39%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -19,7 +19,10 @@ class Solution {
         return symmetry(root.left,root.right);
     }
     public boolean symmetry(TreeNode root1 ,TreeNode root2){
-        if(root1==null || root2==null) return root1==root2;
-        return root1.val == root2.val && symmetry(root1.left,root2.right) && symmetry(root1.right,root2.left);
+        if(root1 == null || root2 == null){
+            return root1 == root2;
+        }
+        if(root1.val != root2.val) return false;
+        return symmetry(root1.left, root2.right) && symmetry(root1.right, root2.left);
     }
 }
